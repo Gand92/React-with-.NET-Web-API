8b7c965ae0f56f5c37c76278155d775d0d3dbc63
Alessandro <alessandro.rotili@gmail.com>
2023-04-12 22:11:42 +0200

    Added base drag and drop functions

--- RankingApp/RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/RankingApp/ClientApp/src/components/RankItems.js
@@ -7,6 +7,28 @@ const RankItems = () => {
     const [items, setItems] = useState([]);
     const dataType = 1;
 
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map(item => (item.id === parseInt(data)) ?
+                                                {...item, ranking: parseInt(targetElm.id.substring(5))} : {...item, ranking:item.ranking});
+            setItems(transformedCollection);
+        }
+    }
+
     useEffect(() => {
         //fetch method is used to make an get http request to the server - item is what is declared inside setupProxy context
         fetch('item/'+ dataType)
@@ -20,7 +42,7 @@ const RankItems = () => {
 
     return (
         <main>
-            <RankingGrid items={items} imgArr={MovieImageArr} />
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop}/>
             <div className = "items-not-ranked">
             {
                     (items.length > 0) ? items.map((item) =>
--- RankingApp/RankingApp/ClientApp/src/components/RankingGrid.js
+++ RankingApp/RankingApp/ClientApp/src/components/RankingGrid.js
@@ -1,4 +1,4 @@
-﻿const RankingGrid = ({items, imgArr }) => {
+﻿const RankingGrid = ({items, imgArr, drag, allowDrop, drop }) => {
 
     const rankingGrid = [];
     const cellCollectionTop = [];
@@ -9,7 +9,10 @@
     function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) {
         if (rankNum > 0) {
             var item = items.find(o => o.ranking === rankNum);
-            cellCollection.push(<div id={'rank-' + rankNum} className="rank-cell"></div>);
+            cellCollection.push(<div id={'rank-' + rankNum} onDrop={drop} onDragOver={allowDrop} className="rank-cell">
+                {(item != null) ? <img id={`item-${item.id}`} src={imgArr.find(o => o.id === item.imageId)?.image} draggable="true" onDragStart={drag} />
+                                : null}
+            </div>);
         } else {
             cellCollection.push(<div className="row-label">
                 <h4>{rowLabel}</h4>
